c47074f0fc6401ee0deb415d3efe97715142e94e
TrishaalaSharma23-design <trishaala.sharma129603@marwadiuniversity.ac.in>
2026-04-15 00:06:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -75,10 +75,6 @@ app.use("/listings" , listingsRouter);
 app.use("/listings/:id/reviews" , reviewsRouter);
 app.use("/" , userRouter);
 
-app.get("/" , (req , res) => {
-    res.redirect("/listings");
-})
-
 //IF ANY INVALID ROUTE IS SEARCHED
 app.use((req, res, next) => {
     next(new ExpressError(404, "PAGE NOT FOUND"));
