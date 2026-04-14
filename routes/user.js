9b6207ab199f4ccd2d96dba73ca73dfd375a02a5
TrishaalaSharma23-design <trishaala.sharma129603@marwadiuniversity.ac.in>
2026-04-14 23:37:00 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
 const passport = require("passport");
-const { saveRedirectUrl } = require("../middleware.js");
+const { saveRedirectUrl } = require("../middleware_disabled.js");
 const userController = require("../controllers/users.js")
 
 router
